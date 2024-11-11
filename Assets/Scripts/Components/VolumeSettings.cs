9d0b863359c6de700bde65b5dce75327e432a2cf
ariannfarias <ariannfarias@planetbids.com>
2024-11-11 09:14:35 -0300

    Audio Slider

---
+++ Assets/Scripts/Components/VolumeSettings.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicSlider;
+
+    private void Start() {
+        LoadVolume();
+        
+        SetMusicVolume();
+    }
+
+    public void SetMusicVolume() 
+    {
+        float volume = musicSlider.value;
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        }
+    }
+}
