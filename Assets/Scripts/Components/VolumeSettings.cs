1a92e92ce1ae4025a430a5bb8127325d3e5547cc
ariannfarias <ariannmichael@gmail.com>
2024-11-19 17:39:42 -0300

    OptionsMenu

--- Assets/Scripts/Components/VolumeSettings.cs
+++ Assets/Scripts/Components/VolumeSettings.cs
@@ -6,6 +6,7 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
 
     private void Start() {
         LoadVolume();
@@ -20,11 +21,22 @@ public class VolumeSettings : MonoBehaviour
         PlayerPrefs.SetFloat("MusicVolume", volume);
     }
 
+    public void SetSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
     private void LoadVolume()
     {
         if (PlayerPrefs.HasKey("MusicVolume"))
         {
             musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        }
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("SFXVolume");
         }
     }
 }
